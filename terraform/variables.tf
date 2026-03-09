423ad48338e69f9e0ee9b28529d273159852a0ab
Sush-Sharma <101503738+Sush-Sharma@users.noreply.github.com>
2026-03-10 03:22:46 +0530

    Create variables.tf

---
+++ terraform/variables.tf
@@ -0,0 +1,14 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  default     = "cloud-practice-1-488818"
+}
+
+variable "region" {
+  description = "GCP Region"
+  default     = "us-central1"
+}
+
+variable "zone" {
+  description = "GCP Zone"
+  default     = "us-central1-a"
+}
