4dc457b5609b3ea967f3aa6097c0f4c65a7400b3
Sush-Sharma <101503738+Sush-Sharma@users.noreply.github.com>
2026-03-11 04:09:14 +0530

    Update startup-script.sh

--- scripts/startup-script.sh
+++ scripts/startup-script.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
 
+apt update
+apt install -y apache2
+
+cat <<EOF > /var/www/html/index.html
+<h1>Scalable Web Server Running on Google Cloud</h1>
+<p>Deployed using Terraform</p>
+EOF
+
+systemctl restart apache2
