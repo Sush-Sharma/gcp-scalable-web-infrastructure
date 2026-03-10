12f3399c2b02edddd0938263e64545999b4c35ec
Sush-Sharma <101503738+Sush-Sharma@users.noreply.github.com>
2026-03-11 03:57:31 +0530

    Delete startup-script.sh

--- startup-script.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-apt update
-apt install apache2 -y
-systemctl start apache2
-systemctl enable apache2
-
-echo "<h1>Scalable Web Server</h1>" > /var/www/html/index.html
-echo "<p>Instance: $(hostname)</p>" >> /var/www/html/index.html
